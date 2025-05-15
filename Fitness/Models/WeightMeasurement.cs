8d1d217b8886c4a962a1af7984f3c7c041999ffc
eryk <void5436@gmail.com>
2025-05-15 13:28:14 +0200

    fix validation errors messages

--- Fitness/Models/WeightMeasurement.cs
+++ Fitness/Models/WeightMeasurement.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Fitness.Models;
 
 public class WeightMeasurement
 {
     public int Id { get; set; }
+    [Required]
     public DateOnly Date { get; set; }
+    [Required]
     public float Weight { get; set; }
 }
